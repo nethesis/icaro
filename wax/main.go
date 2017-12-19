5e4aa461dd77182fd85bf5d786c15e246ae49643
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-12-19 12:17:43 +0100

    icaro. refactoring names and dirs

---
+++ wax/main.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * This file is part of Windmill project.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ *
+ * author: Edoardo Spadoni <edoardo.spadoni@nethesis.it>
+ */
+
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"sun-api/configuration"
+	"sun-api/methods"
+)
+
+func main() {
+	// read and init configuration
+	configuration.Init()
+
+	// init routers
+	router := gin.Default()
+
+	api := router.Group("/api")
+
+	api.GET("/aaa-login", methods.Login)
+
+	router.Run()
+
+}
