67c32ab1b784b59f3f5ac5c4d9c81df9bbb13d0d
endritkane <endritkane95@gmail.com>
2017-12-20 17:43:24 +0100

    change components files for login

--- sun/sun-ui/src/app/login/login.component.ts
+++ sun/sun-ui/src/app/login/login.component.ts
@@ -6,7 +6,8 @@ import { error } from 'util';
 @Component({
     moduleId:module.id,
     selector: 'router-outlet',
-    templateUrl:'login.component.html'
+    templateUrl:'login.component.html',
+    styleUrls: ['login.component.css']
 })
 
 
