ed1fadc9d6c5abbdb4412816deed923e383bbd67
endritkane <endritkane95@gmail.com>
2017-12-21 16:49:51 +0100

    added sidenav and navbar

---
+++ sun/sun-ui/src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'sidenav',
+	templateUrl: 'sidenav.component.html',
+	styleUrls: ['sidenav.component.css']
+})
+export class SidenavComponent { }
