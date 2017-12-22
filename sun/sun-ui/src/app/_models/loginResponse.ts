5b9815f401cf7fc03a45547a521a2e64328423bb
MikelKasneci <m.kasneci@evolutivo.it>
2017-12-22 11:39:18 +0100

    # This is a combination of 3 commits. # This is the 1st commit message:
    Refactor Login Part
    
    # This is the commit message #2:
    
    Fixed absolute path
    
    # This is the commit message #3:
    
    Squash 3 lastes commits

---
+++ sun/sun-ui/src/app/_models/loginResponse.ts
@@ -0,0 +1,6 @@
+export interface loginResponse{
+    account_type:string;
+    expires:string;
+    status:string;
+    token:string;
+}
--- sun/sun-ui/src/app/_services/authentication.service.ts
+++ sun/sun-ui/src/app/_services/authentication.service.ts
@@ -3,23 +3,29 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Headers, RequestOptions}    from '@angular/http'
 import 'rxjs/add/operator/map';
+import { Subject } from 'rxjs/Subject';
 
 
 @Injectable()
 export class AuthenticationService  {
+    public homeObject = new Subject<any>();
+    
     constructor(private http:HttpClient){}
+
+    /**
+     * Make the Login Call in API
+     * @param username 
+     * @param password 
+     */
     login (username:string, password:string){
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        
-        return  this.http.post<any>('http://localhost:6900/api/login/',{username:username,password:password
+        return  this.http.post<any>('http://hstest.neth.eu:8080/api/login',{username:username,password:password
         })
-        .map(user=>{
-            //login succesful if there is a jwk token in response
-            if(user && user.token){
-                localStorage.setItem('currentUser', JSON.stringify(user));
-            }
-            return user;
-        });
+        .map(res => res);
+    }
+
+    addDataHome(data:any){
+        this.homeObject.next(data);
     }
 }
