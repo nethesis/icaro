35afeb9c04e04642beb151acda505dfbd07a55c6
MikelKasneci <m.kasneci@evolutivo.it>
2017-12-22 14:25:11 +0100

    Working on manage account

--- sun/sun-ui/src/app/login/login.component.ts
+++ sun/sun-ui/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
 import {Http, Headers} from '@angular/http';
 import { AuthenticationService } from '../_services/authentication.service';
+import { loginResponse } from '../_models/loginResponse';
 @Component({
     moduleId:module.id,
     selector: 'login',
@@ -13,22 +14,28 @@ import { AuthenticationService } from '../_services/authentication.service';
 
 
 export class LoginComponent{
+    home : Array<any> = [];
     model: any = {};
     responseApi:string;
     constructor (
-        public router: Router, public http: Http, private authenticationService: AuthenticationService
+        public router: Router, public http: Http, private authenticationService: AuthenticationService, 
     ){}
 
     login() {
       this.authenticationService.login(this.model.username, this.model.password)
           .subscribe(
               data => {
+                  this.home = data;
                   console.log(data);
                   this.router.navigate(['/home']);
               },
               error => {
-                console.log(error);
+                console.log(error.error.message);
               });
+
+            this.authenticationService.homeObject.subscribe(
+                data => this.home = [data, ...this.home]
+            );
   }
 
 
