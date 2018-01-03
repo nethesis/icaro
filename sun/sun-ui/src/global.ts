b3e7ebc259a88c4d304b3c73522285446562ef38
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-03 10:03:50 +0100

    sun-ui. Changed the global variables

--- sun/sun-ui/src/global.ts
+++ sun/sun-ui/src/global.ts
@@ -1,5 +1,5 @@
 export const apConfig = Object.freeze({
   LOGO_URL: '/assets/images/login-images/nethesis-logo.png',
-  MAIN_TITLE: 'Nethesis Operation Center',
-  API_ENDPOINT_URL: 'http://hstest.neth.eu:8080/api/login'
+  MAIN_TITLE: 'Icaro',
+  API_ENDPOINT_URL: ''
 });
