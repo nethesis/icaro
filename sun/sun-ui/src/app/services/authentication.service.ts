b330b7d3078dd90bd5f6a11706b92d7d4f55306b
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-12 19:11:41 +0100

    sun-ui. Modified home component services

--- sun/sun-ui/src/app/services/authentication.service.ts
+++ sun/sun-ui/src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { apConfig } from './../../global';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { HttpHeaders } from '@angular/common/http/';
 
 @Injectable()
 export class AuthenticationService {
@@ -15,16 +16,21 @@ export class AuthenticationService {
    * @param password
    */
   authentication(username: string, password: string) {
+    let myHeader = new HttpHeaders();
+    myHeader.append('Content-Type', 'application/json');
+    let obj = {
+      username: username,
+      password: password
+    };
+    let data = JSON.stringify(obj);
     return this.http
-      .post<any>(this.apiLoginUrl, {
-        username: username,
-        password: password
-      })
+      .post<any>(this.apiLoginUrl, data, { headers: myHeader })
       .map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
           // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('currentUser', JSON.stringify(user));
+          localStorage.setItem('authenticationToken', user.token);
         }
         return user;
       });
