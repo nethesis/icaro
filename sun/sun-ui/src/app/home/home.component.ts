7a3508d4c677639790aa8cf81dfcfc0305280a76
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-15 18:44:24 +0100

    sun-ui. Changed home design

--- sun/sun-ui/src/app/home/home.component.ts
+++ sun/sun-ui/src/app/home/home.component.ts
@@ -1,20 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './../services/profile.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './../services/authentication.service';
+import { apConfig } from '../../global';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  toggleOnClick: true;
+  mainTitle = apConfig.MAIN_TITLE;
+  logo = apConfig.LOGO_URL;
   public account_type = localStorage.getItem('account_type');
+  email: string;
   constructor(
     private authenticationService: AuthenticationService,
+    private profileService: ProfileService,
     private router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.profileService.getAccount(5).subscribe(
+      data => {
+        for (let key in data) {
+          if (key === 'email') {
+            this.email = data[key];
+          }
+        }
+        console.log(this.email);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
   /**
    * This method, call logout procedure from service part.
@@ -22,5 +44,17 @@ export class HomeComponent implements OnInit {
   logOut() {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
+  }
+
+  /**
+   * Toggle sidebar
+   * Gall specific width to sidebar and main content.
+   */
+  toggleSidebar() {
+    if (this.toggleOnClick) {
+      return '185' + 'px';
+    } else {
+      return '75' + 'px';
+    }
   }
 }
