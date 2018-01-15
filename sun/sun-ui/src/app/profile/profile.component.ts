a2569c4524da6b59a411a35d5067ef7ab962fbfd
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-15 18:46:49 +0100

    sun-ui. Changed profile component

--- sun/sun-ui/src/app/profile/profile.component.ts
+++ sun/sun-ui/src/app/profile/profile.component.ts
@@ -1,6 +1,10 @@
+import { NgForm } from '@angular/forms';
 import { ProfileService } from './../services/profile.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CardConfig, InfoStatusCardConfig } from 'patternfly-ng';
+import { Account, AccountFull } from '../interfaces/account';
+import { error } from 'util';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -10,26 +14,72 @@ import { CardConfig, InfoStatusCardConfig } from 'patternfly-ng';
 })
 export class ProfileComponent implements OnInit {
   config: CardConfig;
-  public account_type = localStorage.getItem('account_type');
+  model: any = {};
+  accountArray: any[] = [];
+  account: any = {};
+  fullAccount: any = {};
+  preferenceArray: any[] = [];
+  passwordLength: number;
+  apiResponse: string;
 
-  constructor(private profileService: ProfileService) {}
+  constructor(
+    private profileService: ProfileService,
+    private activateroute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.passwordLength = 0;
+  }
 
   ngOnInit() {
-    this.profileService.getAccounts();
     this.config = {
       noPadding: false,
       topBorder: true
     } as CardConfig;
+    this.getAccount();
+    this.getPreferences();
   }
 
-  private getAllAccounts() {
-    this.profileService.getAccounts().subscribe(
+  private postAccount() {
+    this.fullAccount.email = 'test@hotmail.com';
+    this.fullAccount.name = 'Test';
+    this.fullAccount.username = 'Test';
+    this.fullAccount.uuid = '29';
+    this.fullAccount.hotspotid = '29';
+    this.fullAccount.password = 'password';
+    this.fullAccount.type = 'type1';
+
+    this.profileService.postAccount(this.fullAccount).subscribe(
       data => {
         console.log(data);
       },
       error => {
         console.log(error);
       }
+    );
+  }
+
+  private getAccount() {
+    this.profileService.getAccount(5).subscribe(
+      data => {
+        this.accountArray = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error.json());
+      }
+    );
+  }
+
+  private getPreferences() {
+    this.profileService.getPreferences().subscribe(
+      data => {
+        let myArray = [];
+        for (let key in error) {
+          myArray.push(data[key]);
+        }
+        this.preferenceArray = myArray;
+      },
+      error => {}
     );
   }
 }
