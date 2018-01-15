4c1a302a5dd6f50e7a57e22728cfab03f27f60e7
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-15 18:47:42 +0100

    sun-ui. Implemented services for profile

--- sun/sun-ui/src/app/services/profile.service.ts
+++ sun/sun-ui/src/app/services/profile.service.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../header/header.service';
 import { RequestOptions, Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { apConfig } from '../../global';
@@ -5,18 +6,60 @@ import { apConfig } from '../../global';
 @Injectable()
 export class ProfileService {
   private apiAccountsUrl = apConfig.API_ENDPOINT_URL + '/accounts';
+  private apiPreferencesUrl = apConfig.API_ENDPOINT_URL +
+    '/preferences/accounts';
   private authenticationToken = localStorage.getItem('authenticationToken');
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private headerService: HeaderService) {}
 
-  getAccounts() {
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Token', this.authenticationToken);
-    let myParams = new URLSearchParams();
-    let options = new RequestOptions({ headers: myHeaders, params: myParams });
+  /**
+   * Get Accounts list from API
+   */
+  getAllAccounts() {
     return this.http
-      .get(this.apiAccountsUrl, options)
-      .map(res => res);
+      .get(this.apiAccountsUrl, this.headerService.setHeader())
+      .map(res => res.json());
+  }
+
+  /**
+   * Get Account in base of id
+   * @param id
+   */
+  getAccount(id: number) {
+    return this.http
+      .get(this.apiAccountsUrl + '/' + id, this.headerService.setHeader())
+      .map(res => res.json());
+  }
+
+  /**
+   * Make the callwhich change password
+   * @param account
+   */
+  changePassword(account: any) {
+    let body = JSON.stringify(account);
+    return this.http
+      .put(
+        this.apiAccountsUrl + '/' + '5',
+        body,
+        this.headerService.setHeader()
+      )
+      .map(res => res.json());
+  }
+
+  /**
+   * Make the call which create a new account
+   * @param account
+   */
+  postAccount(account: any) {
+    let body = JSON.stringify(account);
+    return this.http
+      .post(this.apiAccountsUrl, body, this.headerService.setHeader())
+      .map(res => res.json());
+  }
+
+  getPreferences() {
+    return this.http
+      .get(this.apiPreferencesUrl, this.headerService.setHeader())
+      .map(res => res.json());
   }
 }
