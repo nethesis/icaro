b9658cf140af9b9de5f2f09ca634dbe6ec7e5695
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-16 16:58:38 +0100

    sun-ui. Implemented hide modal or display error when submit

--- sun/sun-ui/src/app/modals/change-password-modal/change-password-modal.component.ts
+++ sun/sun-ui/src/app/modals/change-password-modal/change-password-modal.component.ts
@@ -1,8 +1,10 @@
 import { NgForm } from '@angular/forms';
 import { ProfileService } from '../../services/profile.service';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef} from '@angular/core';
 import { error } from 'util';
+import * as jQuery from 'jquery';
 
+declare var $: any;
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-change-password-modal',
@@ -10,6 +12,8 @@ import { error } from 'util';
   styleUrls: ['./change-password-modal.component.css']
 })
 export class ChangePasswordModalComponent implements OnInit {
+  @ViewChild('myModal') myModal: ElementRef;
+  private id_profile = Number(localStorage.getItem('id'));
   model: any = {};
   items: any[] = [];
   account: any = {};
@@ -20,29 +24,47 @@ export class ChangePasswordModalComponent implements OnInit {
 
   constructor(private profileService: ProfileService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+     this.getAccount(this.id_profile);
+  }
 
   private getPasswordLength(value: string = '') {
     this.passwordLength = value.length;
   }
 
   private changePassword(changepasswordform: NgForm) {
-    this.account.email = 'test@hotmail.com';
-    this.account.name = 'test';
-    this.account.username = 'test';
+   this.account.email = this.fullAccount.email;
+    this.account.name = this.fullAccount.name;
+    this.account.username = this.fullAccount.username;
     this.account.password = this.model.newPassword;
-    this.account.id = 5;
+    this.account.id = this.fullAccount.id;
     this.profileService.changePassword(this.account).subscribe(
       data => {
         this.apiResponse = data.status;
         changepasswordform.reset();
         this.passwordLength = 0;
+        if (this.apiResponse === 'success') {
+          $(this.myModal.nativeElement).modal('hide');
+        }
+      },
+      error => {
+        this.apiResponse = 'error';
+        console.log(error);
+      }
+    );
+  }
+
+  private getAccount(id:number) {
+    this.profileService.getAccount(id).subscribe(
+      data => {
+        this.fullAccount = data;
       },
       error => {
         console.log(error);
       }
     );
   }
+
 
   private resetFields(changepasswordform: NgForm) {
     changepasswordform.reset();
