b400794e96d6254dd0103073ca7059d5b35cfb70
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2018-02-02 11:34:16 +0100

    Sun UI: implement basic dashboard

--- sun/sun-ui/src/components/Dashboard.vue
+++ sun/sun-ui/src/components/Dashboard.vue
@@ -6,15 +6,14 @@
       <div class="col-xs-12 col-sm-6 col-md-3">
         <div class="card-pf card-pf-accented card-pf-aggregate-status">
           <h2 class="card-pf-title">
-            <span class="fa fa-shield"></span>
-            <span class="card-pf-aggregate-status-count">0</span> Ipsum
+            <span class="fa fa-wifi"></span>
+            {{ $t("dashboard.hotspots") }}
           </h2>
           <div class="card-pf-body">
             <p class="card-pf-aggregate-status-notifications">
               <span class="card-pf-aggregate-status-notification">
-                <a href="#" class="add" data-toggle="tooltip" data-placement="top" title="Add Ipsum">
-                  <span class="pficon pficon-add-circle-o"></span>
-                </a>
+                <div v-if="totals.hotspots.isLoading" class="spinner spinner-sm"></div>
+                <div v-if="!totals.hotspots.isLoading">{{ totals.hotspots.count }}</div>
               </span>
             </p>
           </div>
@@ -24,70 +23,71 @@
       <div class="col-xs-12 col-sm-6 col-md-3">
         <div class="card-pf card-pf-accented card-pf-aggregate-status">
           <h2 class="card-pf-title">
-            <a href="#">
-              <span class="fa fa-shield"></span>
-              <span class="card-pf-aggregate-status-count">20</span> Amet</a>
+              <span class="fa pficon-users"></span>
+              {{ $t("dashboard.accounts") }}
           </h2>
           <div class="card-pf-body">
             <p class="card-pf-aggregate-status-notifications">
               <span class="card-pf-aggregate-status-notification">
-                <a href="#">
-                  <span class="pficon pficon-error-circle-o"></span>4</a>
-              </span>
-              <span class="card-pf-aggregate-status-notification">
-                <a href="#">
-                  <span class="pficon pficon-warning-triangle-o"></span>1</a>
+                <div v-if="totals.accounts.isLoading" class="spinner spinner-sm"></div>
+                <div v-if="!totals.accounts.isLoading">{{ totals.accounts.count }}</div>
               </span>
             </p>
           </div>
         </div>
 
       </div>
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <div class="card-pf card-pf-accented card-pf-aggregate-status">
-          <h2 class="card-pf-title">
-            <a href="#">
-              <span class="fa fa-shield"></span>
-              <span class="card-pf-aggregate-status-count">9</span> Adipiscing</a>
-          </h2>
-          <div class="card-pf-body">
-            <p class="card-pf-aggregate-status-notifications">
-              <span class="card-pf-aggregate-status-notification">
-                <span class="pficon pficon-ok"></span>
-              </span>
-            </p>
-          </div>
-        </div>
 
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <div class="card-pf card-pf-accented card-pf-aggregate-status">
-          <h2 class="card-pf-title">
-            <a href="#">
-              <span class="fa fa-shield"></span>
-              <span class="card-pf-aggregate-status-count">12</span> Lorem</a>
-          </h2>
-          <div class="card-pf-body">
-            <p class="card-pf-aggregate-status-notifications">
-              <a href="#">
-                <span class="card-pf-aggregate-status-notification">
-                  <span class="pficon pficon-error-circle-o"></span>1</span>
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import HotspotService from '../services/hotspot';
+  import AccountService from '../services/account';
+  import StorageService from '../services/storage';
+
   export default {
     name: 'Dashboard',
+    mixins: [HotspotService, AccountService, StorageService],
     data() {
+
+      // get totals
+      this.getTotals();
+
+
       return {
-        msg: 'Dashboard'
+        msg: 'Dashboard',
+        totals: {
+          accounts: {
+            isLoading: true,
+            count: 0
+          },
+          hotspots: {
+            isLoading: true,
+            count: 0
+          }
+        }
       }
+    },
+    methods: {
+      getTotals() {
+        this.hotspotGetAll(success => {
+          this.totals.hotspots.count = success.body.length
+          this.totals.hotspots.isLoading = false
+        }, error => {
+          console.log(error.body)
+          this.totals.hotspots.isLoading = false
+        })
+        this.accountGetAll(success => {
+          this.totals.accounts.count = success.body.length
+          this.totals.accounts.isLoading = false
+        }, error => {
+          console.log(error.body)
+          this.totals.accounts.isLoading = false
+        })
+      },
+
     }
   }
 </script>
