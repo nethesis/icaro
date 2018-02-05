04b7b7617852f44bb1820b7a3ec3acebc5ba7aff
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-02-05 09:21:13 +0100

    sun-ui. moved uuid gen to utils service

--- sun/sun-ui/src/services/util.js
+++ sun/sun-ui/src/services/util.js
@@ -9,6 +9,15 @@ var UtilService = {
         globalSearchPlaceholder: this.$i18n.t('search'),
       }
     },
+    generateUUID() {
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+    },
   }
 };
 export default UtilService;
