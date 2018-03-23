e75dac1df3dfaecf506ebcd61e16e0e8f892634e
Matteo Valentini <matteo.valentini@nethesis.it>
2018-03-23 09:15:43 +0100

    wax. refactor login code

--- wax/methods/login.go
+++ wax/methods/login.go
@@ -26,10 +26,13 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/nethesis/icaro/sun/sun-api/database"
+	"github.com/nethesis/icaro/sun/sun-api/models"
 	"github.com/nethesis/icaro/wax/utils"
 )
 
@@ -46,6 +49,21 @@ func AuthReject(c *gin.Context, description string) {
 
 	c.String(http.StatusForbidden, message)
 	c.Abort()
+}
+
+func isAutoLoginEnabled(hotspotId int) bool {
+	var hotspot_pref models.HotspotPreference
+	isEnabled := false
+
+	db := database.Database()
+	db.Where("hotspot_id = ? and `key` = ?", hotspotId, "auto_login").First(&hotspot_pref)
+	db.Close()
+
+	if hotspot_pref.Id != 0 {
+		isEnabled, _ = strconv.ParseBool(hotspot_pref.Value)
+	}
+
+	return isEnabled
 }
 
 func autoLogin(c *gin.Context, unitMacAddress string, username string, userMac string, sessionId string) {
@@ -125,4 +143,33 @@ func Login(c *gin.Context, unitMacAddress string, username string, chapPass stri
 	// response to dedalo
 	AuthAccept(c, outPrefs.String())
 
+}
+
+func Logins(c *gin.Context) {
+
+	service := c.Query("service")
+	switch service {
+	case "framed":
+		unit := utils.GetUnitByMacAddress(c.Query("ap"))
+		if isAutoLoginEnabled(unit.HotspotId) {
+			unitMacAddress := c.Query("ap")
+			user := c.Query("user")
+			userMac := c.Query("mac")
+			sessionId := c.Query("sessionid")
+			autoLogin(c, unitMacAddress, user, userMac, sessionId)
+		} else {
+			c.String(http.StatusForbidden, "Autologin disabled")
+		}
+
+	case "login":
+		unitMacAddress := c.Query("ap")
+		user := c.Query("user")
+		chapPass := c.Query("chap_pass")
+		chapChal := c.Query("chap_chal")
+		sessionId := c.Query("sessionid")
+		Login(c, unitMacAddress, user, chapPass, chapChal, sessionId)
+
+	default:
+		c.String(http.StatusNotFound, "Invalid login service: '%s'", service)
+	}
 }
