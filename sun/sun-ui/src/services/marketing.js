c9aeadb79577875136ba54d7c7f6d89279d597f0
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-03-07 12:17:34 +0100

    sun-ui. added marketing page

---
+++ sun/sun-ui/src/services/marketing.js
@@ -0,0 +1,34 @@
+var MarketingService = {
+  methods: {
+    marketingPrefGet(id, success, error) {
+      this.$http
+        .get(
+          this.$root.$options.api_scheme +
+          this.$root.$options.api_host +
+          "/api/marketing/" +
+          id, {
+            headers: {
+              Token: (this.get("loggedUser") && this.get("loggedUser").token) || ""
+            }
+          }
+        )
+        .then(success, error);
+    },
+    marketingPrefModify(id, body, success, error) {
+      this.$http
+        .put(
+          this.$root.$options.api_scheme +
+          this.$root.$options.api_host +
+          "/api/marketing/" +
+          id,
+          body, {
+            headers: {
+              Token: (this.get("loggedUser") && this.get("loggedUser").token) || ""
+            }
+          }
+        )
+        .then(success, error);
+    }
+  }
+};
+export default MarketingService;
