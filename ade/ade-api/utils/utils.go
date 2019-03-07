becb99517cb42f8fd3f9553c671f008d4c8669d8
Matteo Valentini <matteo.valentini@nethesis.it>
2019-03-07 15:42:45 +0100

    ade-api: add utils for sending email

--- ade/ade-api/utils/utils.go
+++ ade/ade-api/utils/utils.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"strconv"
+
+	gomail "gopkg.in/gomail.v2"
+
 	"github.com/nethesis/icaro/ade/ade-api/models"
+	"github.com/nethesis/icaro/sun/sun-api/configuration"
 	"github.com/nethesis/icaro/sun/sun-api/database"
 	wax_utils "github.com/nethesis/icaro/wax/utils"
 )
@@ -34,4 +40,41 @@ func GetAdeTokenFromToken(token string) models.AdeToken {
 	db.Where("token = ?", token).First(&adeToken)
 
 	return adeToken
+}
+
+func SendFeedBackMessage(hotspotId int, message string) bool {
+	return SendEmail(hotspotId, "User Feedback", message)
+}
+
+func SendReviewMessage(hotspotId int, stars int, message string) bool {
+	stars_s := strconv.Itoa(stars)
+	return SendEmail(hotspotId, "Review: "+stars_s+"/5", message)
+}
+
+func SendEmail(hotspotId int, subject string, message string) bool {
+
+	status := true
+	m := gomail.NewMessage()
+	m.SetHeader("From", configuration.Config.Endpoints.Email.From)
+
+	mail_to := wax_utils.GetHotspotPreferencesByKey(hotspotId, "marketing_2_feedback_email").Value
+
+	m.SetHeader("To", mail_to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", message)
+
+	d := gomail.NewDialer(
+		configuration.Config.Endpoints.Email.SMTPHost,
+		configuration.Config.Endpoints.Email.SMTPPort,
+		configuration.Config.Endpoints.Email.SMTPUser,
+		configuration.Config.Endpoints.Email.SMTPPassword,
+	)
+
+	// send the email
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		status = false
+	}
+
+	return status
 }
