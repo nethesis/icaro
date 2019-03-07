7c918fb598b44df5ce88e22faee2f6dd24e7d7f8
Matteo Valentini <matteo.valentini@nethesis.it>
2019-03-07 10:22:00 +0100

    ade-api. add api for feedback page

---
+++ ade/ade-api/utils/utils.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/nethesis/icaro/ade/ade-api/models"
+	"github.com/nethesis/icaro/sun/sun-api/database"
+	wax_utils "github.com/nethesis/icaro/wax/utils"
+)
+
+func GetHotspotPrefs(hotspotId int) map[string]string {
+	hotspotPerfs := []string{"captive_2_title",
+		"captive_3_logo",
+		"captive_7_background"}
+
+	prefsMap := make(map[string]string)
+
+	prefs := wax_utils.GetHotspotPreferencesByKeys(hotspotId, hotspotPerfs)
+
+	for i := 0; i < len(prefs); i++ {
+		prefsMap[prefs[i].Key] = prefs[i].Value
+	}
+
+	return prefsMap
+}
+
+func GetAdeTokenFromToken(token string) models.AdeToken {
+	var adeToken models.AdeToken
+
+	db := database.Instance()
+	db.Where("token = ?", token).First(&adeToken)
+
+	return adeToken
+}
