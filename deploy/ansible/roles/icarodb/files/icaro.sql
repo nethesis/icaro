192701524fe4d90df913fb16e331a570c9bf62aa
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-02-12 17:58:14 +0100

    deploy. insert business_dpo field

--- deploy/ansible/roles/icarodb/files/icaro.sql
+++ deploy/ansible/roles/icarodb/files/icaro.sql
@@ -55,6 +55,7 @@ CREATE TABLE `hotspots` (
   `business_vat` varchar(512),
   `business_address` varchar(512),
   `business_email` varchar(512),
+  `business_dpo` varchar(512),
   `created` datetime,
   FOREIGN KEY (`account_id`) REFERENCES accounts(`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   KEY(`name`),
