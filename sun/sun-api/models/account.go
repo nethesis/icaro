b46272379d181763d94050ac3bab15a1d7b818e6
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2024-06-11 15:34:48 +0200

    sun-api & sun-ui. added privacy fields

--- sun/sun-api/models/account.go
+++ sun/sun-api/models/account.go
@@ -25,15 +25,21 @@ package models
 import "time"
 
 type Account struct {
-	Id        int       `db:"id" json:"id"`
-	CreatorId int       `db:"creator_id" json:"creator_id"`
-	Uuid      string    `db:"uuid" json:"uuid"`
-	Type      string    `db:"type" json:"type"`
-	Name      string    `db:"name" json:"name"`
-	Username  string    `db:"username" json:"username"`
-	Password  string    `db:"password" json:"-"`
-	Email     string    `db:"email" json:"email"`
-	Created   time.Time `db:"created" json:"created"`
+	Id             int       `db:"id" json:"id"`
+	CreatorId      int       `db:"creator_id" json:"creator_id"`
+	Uuid           string    `db:"uuid" json:"uuid"`
+	Type           string    `db:"type" json:"type"`
+	Name           string    `db:"name" json:"name"`
+	Username       string    `db:"username" json:"username"`
+	Password       string    `db:"password" json:"-"`
+	Email          string    `db:"email" json:"email"`
+	PrivacyName    string    `db:"privacy_name" json:"privacy_name"`
+	PrivacyVAT     string    `db:"privacy_vat" json:"privacy_vat"`
+	PrivacyAddress string    `db:"privacy_address" json:"privacy_address"`
+	PrivacyEmail   string    `db:"privacy_email" json:"privacy_email"`
+	PrivacyDPO     string    `db:"privacy_dpo" json:"privacy_dpo"`
+	PrivacyDPOMail string    `db:"privacy_dpo_mail" json:"privacy_dpo_mail"`
+	Created        time.Time `db:"created" json:"created"`
 }
 
 type AccountJSON struct {
@@ -45,6 +51,12 @@ type AccountJSON struct {
 	Username             string        `json:"username"`
 	Password             string        `json:"password"`
 	Email                string        `json:"email"`
+	PrivacyName          string        `json:"privacy_name"`
+	PrivacyVAT           string        `json:"privacy_vat"`
+	PrivacyAddress       string        `json:"privacy_address"`
+	PrivacyEmail         string        `json:"privacy_email"`
+	PrivacyDPO           string        `json:"privacy_dpo"`
+	PrivacyDPOMail       string        `json:"privacy_dpo_mail"`
 	Created              time.Time     `json:"created"`
 	HotspotId            int           `json:"hotspot_id"`
 	HotspotName          string        `json:"hotspot_name"`
